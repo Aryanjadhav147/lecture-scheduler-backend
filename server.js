c38534c1dda805e120e5bd795bdeea83823db2d4
Aryanjadhav147 <jadhavaryu24@gmail.com>
2025-11-27 19:57:43 +0530

    updated backend

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/instructor', instructorRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
